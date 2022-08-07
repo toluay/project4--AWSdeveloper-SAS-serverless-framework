22e57865ceec8582846e91135b0c6982784903ae
toluay02 <ayantundedayot@gmail.com>
2022-08-07 15:58:49 +0000

    error in the componet frontend correcyed

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '6jdnwkwulc.execute-api'
+const apiId = '6jdnwkwulc'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
